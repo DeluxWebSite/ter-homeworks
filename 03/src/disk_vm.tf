e636c24c27f2c5a457aa7b3c8afd58e59c52b9df
Serge M <sergey.meljnick@yandex.ru>
2025-07-12 16:23:01 +0300

    added files tf

---
+++ 03/src/disk_vm.tf
@@ -0,0 +1,45 @@
+resource "yandex_compute_disk" "default" {
+  count    = 3
+  name     = "disk-name-${count.index}"
+  size     = "8"
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  image_id = "fd8nru7hnggqhs9mkqps"
+
+  labels = {
+    environment = "test"
+  }
+}
+
+# -----------------------------------------
+
+resource "yandex_compute_instance" "storage" {
+
+name = "vm-from-disks"
+platform_id = "standard-v3"
+zone = "ru-central1-a"
+allow_stopping_for_update = "true"
+
+resources {
+cores = 2
+memory = 2
+}
+
+boot_disk {
+initialize_params {
+image_id = "fd8g64rcu9fq5kpfqls0"
+}
+}
+
+  dynamic secondary_disk {
+   for_each = "${yandex_compute_disk.disk.*.id}"
+   content {
+        disk_id = yandex_compute_disk.disk["${secondary_disk.key}"].id
+   }
+}
+
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
